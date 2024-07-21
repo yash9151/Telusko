bfe3c83afd117823bbe828180e555e320cac1326
Yash <yashkhonde222@gmail.com>
2024-07-21 15:32:25 +0530

    Added dynamic conect as parameter from views.py method to rewnder in home page

--- telusko/calc/views.py
+++ telusko/calc/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 def home(request):
     # return HttpResponse("<h1>Hello World</h1>")
-    return render(request, 'home.html')
+    return render(request, 'home.html', {"name":"SUNNY"})
