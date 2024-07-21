893e569861c8e37331c6d34c6c496b2a3e734d0b
Yash <yashkhonde222@gmail.com>
2024-07-21 15:59:03 +0530

    created form in home page and action as add, created add method in views.py to render nr=ew result page

--- telusko/calc/views.py
+++ telusko/calc/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse
 def home(request):
     # return HttpResponse("<h1>Hello World</h1>")
     return render(request, 'home.html', {"name":"SUNNY"})
+
+def add(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    res = val1 + val2
+    return render(request, "result.html", {"result":res})
