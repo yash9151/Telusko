55ae566972a71653e0385101db5c1d63531b6309
Yash <yashkhonde222@gmail.com>
2024-07-22 20:02:07 +0530

    created more objects of destination and passed it to view and used for loop to display the object attributes in UI

--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -6,7 +6,22 @@ def index(request):
 
     dest1 = Destination()
     dest1.name = "Mumbai"
-    dest1.desc = "The City that never sleeps"
+    dest1.desc = "The City hat Never Sleeps"
+    dest1.img = "destination_1.jpg"
     dest1.price = 750
 
-    return render(request, "index.html",{"dest1":dest1})
+    dest2 = Destination()
+    dest2.name = "Pune"
+    dest2.desc = "The City of Education"
+    dest2.img = "destination_2.jpg"
+    dest2.price = 1000
+
+    dest3 = Destination()
+    dest3.name = "Delhi"
+    dest3.desc = "The City with Pollution"
+    dest3.img = "destination_3.jpg"
+    dest3.price = 800
+
+    dests =[dest1, dest2, dest3]
+
+    return render(request, "index.html",{"dests":dests})
