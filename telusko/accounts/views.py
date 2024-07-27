36105eca021e0332121347ee7a1d654b2e2afcc6
Yash <yashkhonde222@gmail.com>
2024-07-27 16:41:30 +0530

    Added login feature, page and in urls redirected to login, created login.py method in views.py

--- telusko/accounts/views.py
+++ telusko/accounts/views.py
@@ -3,6 +3,23 @@ from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/travello')
+        else:
+            messages.info(request, "Invalid Credentials")
+            return redirect("login") 
+    else:
+        return render(request, "login.html")
+
 def register(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
@@ -24,6 +41,7 @@ def register(request):
                                                 email=email,password=password1)
                 user.save();
                 print("User Created")
+                return redirect("login")
         else:
             messages.info(request, "password not matching...")
             return redirect('register')
